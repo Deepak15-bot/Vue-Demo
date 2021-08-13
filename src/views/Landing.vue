aaff179e821630d96d014c2c3d273e8c26b60362
DeepakKushwaha123 <deepak.kushwaha@creolestudios.com>
2021-08-13 18:11:44 +0530

    First commit

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,9 +1,94 @@
 <template>
-  <h1 class="text-center text-white">Landing Page</h1>
+  <section class="section1 p-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 pb-5">
+          <h1 class="text-center text-white">Card Profile</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <a href="#">
+            <div class="card">
+              <h2 class="text-center text-white">Card1</h2>
+            </div>
+          </a>
+        </div>
+        <div class="col-4">
+          <a href="#">
+            <div class="card">
+              <h2 class="text-center text-white">Card2</h2>
+            </div>
+          </a>
+        </div>
+        <div class="col-4">
+          <a href="#">
+            <div class="card">
+              <h2 class="text-center text-white">Card3</h2>
+            </div>
+          </a>
+        </div>
+        <div class="col-4">
+          <a href="#">
+            <div class="card">
+              <h2 class="text-center text-white">Card4</h2>
+            </div>
+          </a>
+        </div>
+        <div class="col-4">
+          <a href="#">
+            <div class="card">
+              <h2 class="text-center text-white">Card5</h2>
+            </div>
+          </a>
+        </div>
+        <div class="col-4">
+          <a href="#">
+            <div class="card">
+              <h2 class="text-center text-white">Card6</h2>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {},
+  methods: {
+    const DataArray = [
+      {
+        id: 1,
+        name: "Deepak",
+      },
+      {
+        id: 2,
+        name: "Neeraj",
+      },
+      {
+        id: 3,
+        name: "Shartak",
+      },
+    ]
+  },
+};
 </script>
 
-<style></style>
+<style>
+a,
+a:hover {
+  text-decoration: none;
+}
+.card {
+  border: 1px solid #fff;
+  padding: 20px;
+  letter-spacing: 1px;
+  margin-bottom: 20px;
+}
+.card:hover {
+  background: rgb(4, 97, 11);
+  border-color: rgb(4, 97, 11);
+}
+</style>
